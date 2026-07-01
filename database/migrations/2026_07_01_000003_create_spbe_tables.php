72429a1b6a3e4b4154b2ec530ad30df8f24f9bc4
icalpurwanto1990-gif <icalpurwanto1990@gmail.com>
2026-07-02 07:14:29 +0800

    fix: write dummy class for empty create_spbe_tables migration

--- database/migrations/2026_07_01_000003_create_spbe_tables.php
+++ database/migrations/2026_07_01_000003_create_spbe_tables.php
@@ -1,3 +1,23 @@
 <?php
 
-// File removed - replaced by AppStatisticResource
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Removed - replaced by AppStatisticResource
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
