d33ab69e5990348486608a1135efab3cf77f9022
icalpurwanto1990-gif <icalpurwanto1990@gmail.com>
2026-07-06 16:34:48 +0800

    fix: correct namespace for ManageSurveyCategories to comply with PSR-4 autoloading

--- app/Filament/Resources/SurveyCategoryResource/Pages/ManageSurveyCategories.php
+++ app/Filament/Resources/SurveyCategoryResource/Pages/ManageSurveyCategories.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Filament\Resources;
+namespace App\Filament\Resources\SurveyCategoryResource\Pages;
 
-use App\Filament\Resources\SurveyCategoryResource\Pages;
+use App\Filament\Resources\SurveyCategoryResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ManageRecords;
 
